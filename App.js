5509b8e0636fd9fcc357986394c008e90888497b
Annala <n1anje00@students.oamk.fi>
2023-02-08 07:10:06 +0200

    Booking app

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Col, Grid } from 'react-native-easy-grid';
+import { useState } from 'react';
+
+
 
 export default function App() {
+
+ const [date, setDate] = useState(new Date());
+ const [startDate, setStartDate] = useState('');
+ const [endDate, setEndDate] = useState('');
+ const [startTime, setStartTime] = useState('');
+ const [endTime, setEndTime] = useState('');
+ const [status, setStatus] = useState('Pick start date and time');
+ const [mode, setMode] = useState('date');
+ const [show, setShow] = useState(false);
+
+
+
+
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
